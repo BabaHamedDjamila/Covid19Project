48253f13c8aaa25444d7beab10ca016c910aaa79
BabaHamedDjamila <bab_djamila@outlook.fr>
2020-06-02 19:10:43 +0000

    modification sur web.php

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,27 @@ Route::get('/', function () {
 
 Auth::routes(); 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/Catégo_idées', 'CategorieController@idée')->name('Catégo_idées');
+//Route::get('/Catégo_idées', 'CategorieController@idée')->name('Catégo_idées');
 Route::get('Catégo_idées', 'CategorieController@index')->name('Catégo_idées');
-Route::get('Catégorie.show', 'CategorieController@show')->name('Catégorie.show');
-Route::get('Catégorie.edit', 'CategorieController@show')->name('Catégorie.edit');
-Route::get('Catégorie', 'CategorieController@destroy')->name('Catégorie');
+//Route::get('Catégorie.show', 'CategorieController@show')->name('Catégorie.show');
+//Route::get('Catégorie.edit', 'CategorieController@show')->name('Catégorie.edit');
+//Route::get('Catégorie', 'CategorieController@destroy')->name('Catégorie');
 
-Route::resource('Catégorie','CategorieController');
-Route::get('/sports', 'CategorieController@sport')->name('sports');
-Route::get('/santés', 'CategorieController@santé')->name('santés');
-Route::get('/signalements', 'SignalementController@signalement')->name('signalements');
+Route::get('/signalement', 'SignaleController@signalement')->name('signalement');
 Route::get('/publications', 'PublicationController@publication')->name('publications');
-Route::get('Categorie.formulaire', 'CategorieController@formulaire');
-Route::post('categorie', 'CategorieController@store');
-Route::get('/formulaire', 'CategorieController@NouvelleCategorie');
+//Route::get('Categorie.formulaire', 'CategorieController@formulaire');
+//Route::post('categorie', 'CategorieController@store');
+//Route::get('/formulaire', 'CategorieController@NouvelleCategorie');
+
 //Route::get('/Catégo_idées', 'CategorieController@listCatégorie');
 //Route::get('/IdéesFiles\idées', 'HomeController@index')->name('home');
-Route::resource('publicationDossier','PublicationController');
-Route::resource('IdeesFiles','IdeeController');
+//Route::resource('publicationDossier','PublicationController');
+
+
+
+
+
+
+//Route::resource('Catégorie','CategorieController');
+//Route::resource('IdeesFiles','IdeeController');
+//Route::resource('SignaleFiles','SignaleController');
